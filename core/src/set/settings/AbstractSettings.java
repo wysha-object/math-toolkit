1348bdf46d3b008f7ede1dbc9e39c2294420c344
wysha-object <wyshazhisishen@yeah.net>
2023-11-28 16:28:27 +0000

    1.0.0
    Signed-off-by: wysha-object <wyshazhisishen@yeah.net>

---
+++ core/src/set/settings/AbstractSettings.java
@@ -0,0 +1,15 @@
+
+package set.settings;
+
+import set.AbstractSetSubpages;
+
+import javax.swing.*;
+
+/**
+ * @author wysha
+ */
+public abstract class AbstractSettings extends AbstractSetSubpages {
+    protected AbstractSettings(String name, JDialog jDialog) {
+        super(name, jDialog);
+    }
+}
---
+++ core/src/set/settings/AppearanceSetting.java
@@ -0,0 +1,46 @@
+
+package set.settings;
+
+import data.NecessaryData;
+import data.Setting;
+import data.Style;
+
+import javax.swing.*;
+import java.util.HashSet;
+import java.util.Objects;
+
+/**
+ * @author wysha
+ */
+public class AppearanceSetting extends AbstractSettings {
+    public JPanel contentPane;
+    private JComboBox<Style> comboBox;
+    private JLabel jLabel;
+    private JPanel setStyle;
+
+    public AppearanceSetting(JDialog jDialog) {
+        super(AppearanceSetting.class.toString(),jDialog);
+        setStyle();
+        comboBox.setModel(new  DefaultComboBoxModel<>(NecessaryData.necessaryData.styles));
+        comboBox.setSelectedItem(NecessaryData.necessaryData.setting.style);
+    }
+    @Override
+    public void onCancel() {
+        jDialog.dispose();
+    }
+
+    @Override
+    public void save() {
+        Setting.setStyle((Style) Objects.requireNonNull(comboBox.getSelectedItem()));
+    }
+
+    private void setStyle() {
+        HashSet<JComponent> jPanels = new HashSet<>();
+        HashSet<JComponent> buttons = new HashSet<>();
+        jPanels.add(contentPane);
+        jPanels.add(setStyle);
+        buttons.add(comboBox);
+        buttons.add(jLabel);
+        Style.setStyle(jPanels,buttons,null);
+    }
+}
